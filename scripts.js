8db79bdf3e3d16ee4c465f41c83619e5ffdda639
Amin7775 <amin.dpi24@gmail.com>
2024-02-23 02:15:33 +0600

    Calculate The Triangle Area And Display It On UI

--- scripts.js
+++ scripts.js
@@ -1,1 +1,39 @@
 console.log("hello")
+// triangle
+function calculateTriangle(){
+    let triangleBaseInput = document.getElementById('triangleBase')
+    triangleBaseText = parseFloat(triangleBaseInput.value) 
+    let triangleHeightInput = document.getElementById('triangleHeight')
+    triangleHeightText = parseFloat(triangleHeightInput.value)
+    // calculate
+    let res = .5 * triangleBaseText * triangleHeightText
+    console.log(res)
+    // display
+    let displayArea = document.getElementById('display-area')
+    displayArea.innerText = res
+}
+
+// rectangle
+function calculateRectangle(){
+
+}
+
+// parallelogram
+function calculateParallelogram(){
+
+}
+
+// Rhombus
+function calculateRhombus(){
+
+}
+
+// pentagon
+function calculatePentagon(){
+
+}
+
+// ellipse
+function calculateEllipse(){
+
+}
